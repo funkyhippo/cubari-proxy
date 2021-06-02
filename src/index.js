88ede7c6ee58fe6ea0e7fbfc56e7c70775b948ab
funkyhippo <funkyhippo@f-ck.me>
2021-06-02 08:10:22 -0700

    Forgot to remove provider.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style/index.css";
-import "lightgallery.js/dist/css/lightgallery.css";
 import App from "./App.js";
-import { LightgalleryProvider } from "react-lightgallery";
 
 ReactDOM.render(
-  <LightgalleryProvider
-    lightgallerySettings={{
-      closable: false,
-      speed: 200,
-      cssEasing: "ease-in-out",
-      loop: false,
-      // settings: https://sachinchoolur.github.io/lightgallery.js/docs/api.html
-    }}
-    galleryClassName="chapter_gallery"
-  >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </LightgalleryProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
